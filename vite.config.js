157c6cd36c3620be1197ee09dd5262151e6e7a46
Santiago Andrade <seyacat@gmail.com>
2026-05-04 14:08:29 -0500

    Migrar a chat.closer.click: CNAME + base relativo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/simple-websocket-chat/',
+  base: './',
   server: {
     port: 5174,
     open: true
